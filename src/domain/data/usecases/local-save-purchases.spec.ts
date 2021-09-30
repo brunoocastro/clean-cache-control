c2dcae883c883f5de3aad35e3d45f4254b293702
brunoocastro <bruno.scastro2012@hotmail.com>
2021-09-30 17:34:50 -0300

    Move SUT creation to a factory helper method

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,36 +1,45 @@
 class LocalSavePurchases {
-  constructor (private readonly cacheStore: CacheStore) {}
+  constructor(private readonly cacheStore: CacheStore) {}
 
-  async save (): Promise<void> {
-    this.cacheStore.delete()
+  async save(): Promise<void> {
+    this.cacheStore.delete();
   }
 }
 
 interface CacheStore {
-delete: () => void
+  delete: () => void;
 }
 
 class CacheStoreSpy implements CacheStore {
-  deleteCallsCount = 0
+  deleteCallsCount = 0;
 
-  delete() : void {
-    this.deleteCallsCount++
+  delete(): void {
+    this.deleteCallsCount++;
   }
 }
 
+type SutTypes = {
+  sut: LocalSavePurchases;
+  cacheStore: CacheStoreSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalSavePurchases(cacheStore);
+
+  return { sut, cacheStore };
+};
+
 describe("LocalSavePurchases", () => {
   test("should not delete cache on sut.init", () => {
-    const cacheStore = new CacheStoreSpy()
-    new LocalSavePurchases(cacheStore);
+    const {cacheStore} = makeSut()
 
-    expect(cacheStore.deleteCallsCount).toBe(0)
+    expect(cacheStore.deleteCallsCount).toBe(0);
   });
-  
+
   test("should delete old cache on sut.save", async () => {
-    const cacheStore = new CacheStoreSpy()
-    const sut = new LocalSavePurchases(cacheStore);
-    await sut.save()
-    expect(cacheStore.deleteCallsCount).toBe(1)
+    const {cacheStore, sut} = makeSut()
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
-
 });
